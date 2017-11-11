67d279424bd25c919b50b6ba2ecc02fdac3d37ae
Mendel <mendel@mendels-mbp.lan>
2017-11-11 11:45:15 -0800

    add structure for game

---
+++ src/player.js
@@ -0,0 +1,15 @@
+class Player {
+    constructor(){
+        this.hand = []
+    }
+    
+    //adds card to hand, passes turn
+    hit() {
+
+    }
+    
+    //returns value to pass turn
+    stay() {
+
+    }
+}
